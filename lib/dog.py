3a8f0ed950331659fc556544e35528dc770d41fb
johney <johneyburke@gmail.com>
2024-05-23 14:04:57 -0600

    passed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    # Remember that all methods use "self." We'll elaborate on this in a moment.
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
